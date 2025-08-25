6780a15b35963cae6210c9ef464ef858b4d75915
tehuanmelo <tehuanmelo@gmail.com>
2025-08-26 00:28:59 +0400

    change coaches placeholder

--- src/components/NumberInput.jsx
+++ src/components/NumberInput.jsx
@@ -13,7 +13,7 @@ function NumberInput({handleOnChange, state, label}) {
         id={`${label}`}
         type="number"
         min="1"
-        placeholder="insert the number of classes..."
+        placeholder="insert the number..."
         value={state}
         onChange={(e) => handleOnChange(e.target.value)}
         required
